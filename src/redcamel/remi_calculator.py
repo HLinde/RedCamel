add test cases for momentum calculations and fix several sign issues in coordinate transforms tests include: - simple cases for linear motion of an electron and ion without fields - simple cases for cyclotron motion of an electron with only magnetic field - unity of back and forth calculation of momenta to position to momenta for photoionisatian and coulomb explosion

src/redcamel/remi_calculator.py
@@ -60,7 +60,7 @@ class RemiCalculator:
 
     @property
     def voltage_difference(self):
-        return self.voltage_ion - self.voltage_electron
+        return self.voltage_electron - self.voltage_ion
 
     @property
     def length_acceleration_total(self):
@@ -75,14 +75,17 @@ class RemiCalculator:
             "p_jet": lambda p: self.jet_momentum(p),
             "p_trans": lambda p: self.transverse_momentum(p),
             "p_long": lambda p: self.longitudinal_momentum(p),
-            "tof": lambda p_long: self.tof(p_long, mass, charge),
+            "tof": lambda tof_accel, tof_drift: tof_accel + tof_drift,
+            ("tof_accel", "tof_drift"): lambda p_long: self.tof_in_parts(p_long, mass, charge),
             ("x", "y", "R"): lambda tof, p_jet, p_trans: {
                 label: func
                 for label, func in zip(
                     ("x", "y", "R"), self.hit_position_xyR(tof, p_jet, p_trans, mass, charge)
                 )
             },
-            "z": lambda p_long, tof: self.position_longitudinal(p_long, tof, mass, charge),
+            "z": lambda p_long, tof, tof_accel, tof_drift: self.position_longitudinal(
+                p_long, tof, tof_accel, tof_drift, mass, charge
+            ),
         }
         return graph
 
@@ -109,32 +112,43 @@ class RemiCalculator:
     def transverse_momentum(self, momentum: sc.Variable):
         return sc.dot(momentum, self.transverse_unitvector)
 
-    def tof(self, momentum_longitudinal: sc.Variable, mass: sc.Variable, charge: sc.Variable):
+    def tof_in_parts(
+        self, momentum_longitudinal: sc.Variable, mass: sc.Variable, charge: sc.Variable
+    ):
         momentum_longitudinal = momentum_longitudinal.to(unit="N*s")
         mass = mass.to(unit="kg")
         charge = charge.to(unit="C")
         acceleration_direction = np.sign(charge.value * self.electric_field.value)
         if acceleration_direction > 0:
+            length_acceleration = self.length_acceleration_ion
+            length_drift = self.length_drift_ion
+        elif acceleration_direction < 0:
             length_acceleration = self.length_acceleration_electron
             length_drift = self.length_drift_electron
         else:
-            length_acceleration = self.length_acceleration_ion
-            length_drift = self.length_drift_ion
-
+            raise NotImplementedError("Calculation without electric field not implemented")
         voltage = -acceleration_direction * length_acceleration * self.electric_field
+        potential_energy = -voltage * charge
         # TODO add case where particle overcomes opposite acceleration step
-        d = momentum_longitudinal * momentum_longitudinal - 2 * charge * voltage * mass
+        d = momentum_longitudinal**2 + 2 * potential_energy * mass
         root_d = sc.sqrt(d)
-        tof = sc.where(
+
+        tof_accel = sc.where(
             d < 0 * sc.Unit("J*kg"),
-            sc.scalar(np.nan, unit="s"),
-            mass
-            * (
-                2 * length_acceleration / (root_d + acceleration_direction * momentum_longitudinal)
-                + length_drift / root_d
-            ),
+            sc.scalar(np.nan, unit="ns"),
+            (
+                mass
+                * 2
+                * length_acceleration
+                / (root_d + acceleration_direction * momentum_longitudinal)
+            ).to(unit="ns"),
         )
-        return tof.to(unit="ns")
+        tof_drift = sc.where(
+            d < 0 * sc.Unit("J*kg"),
+            sc.scalar(np.nan, unit="ns"),
+            (mass * length_drift / root_d).to(unit="ns"),
+        )
+        return {"tof_accel": tof_accel, "tof_drift": tof_drift}
 
     def hit_position_xyR(
         self,
@@ -149,13 +163,13 @@ class RemiCalculator:
         assert p_x.dims == p_y.dims
         dims = p_x.dims
 
-        # cyclotron motion or linear motion?
+        # Cyclotron motion or linear motion?
         if sc.abs(self.magnetic_field) > 0 * sc.Unit("T"):
             p_xy = sc.sqrt(p_x**2 + p_y**2)
             phi = sc.atan2(x=p_x, y=p_y)  # angle in xy-plane towards jet-direction
             omega = self.calc_omega(mass, charge)
 
-            # alpha/2 has to be periodic in 1*pi!
+            # Alpha/2 has to be periodic in 1*pi!
             # sign of alpha is important as it gives the direction of deflection
             # The sign has to be included also in the modulo operation!
             alpha = (omega.to(unit="1/s") * tof.to(unit="s")).values
@@ -167,7 +181,7 @@ class RemiCalculator:
             R = (2 * p_xy * sc.sin(alpha / 2)) / (charge * self.magnetic_field)
             x = R * sc.cos(theta)
             y = R * sc.sin(theta)
-        else:  # for small magnetic field it reduces to this linear motion:
+        else:  # For small magnetic field it reduces to this linear motion:
             v_x = p_x / mass
             v_y = p_y / mass
             x = v_x * tof
@@ -175,52 +189,27 @@ class RemiCalculator:
             R = sc.sqrt(x**2 + y**2)
         return x.to(unit="mm"), y.to(unit="mm"), R.to(unit="mm")
 
-    def tof_in_acceleration_part(
-        self, momentum_longitudinal: sc.Variable, mass: sc.Variable, charge: sc.Variable
-    ):
-        momentum_longitudinal = momentum_longitudinal.to(unit="N*s")
-        mass = mass.to(unit="kg")
-        charge = charge.to(unit="C")
-        acceleration_direction = np.sign(charge.value * self.electric_field.value)
-        if acceleration_direction > 0:
-            length_acceleration = self.length_acceleration_electron
-        else:
-            length_acceleration = self.length_acceleration_ion
-
-        voltage = -acceleration_direction * length_acceleration * self.electric_field
-        # TODO add case where particle overcomes opposite acceleration step
-        d = momentum_longitudinal * momentum_longitudinal - 2 * charge * voltage * mass
-        root_d = sc.sqrt(d)
-        tof = sc.where(
-            d < sc.scalar(0, unit="J*kg"),
-            sc.scalar(np.nan, unit="s"),
-            mass
-            * (2 * length_acceleration / (root_d + acceleration_direction * momentum_longitudinal)),
-        )
-        return tof.to(unit="ns")
-
     def position_longitudinal(
         self,
         momentum_longitudinal: sc.Variable,
         tof: sc.Variable,
+        tof_accel: sc.Variable,
+        tof_drift: sc.Variable,
         mass: sc.Variable,
         charge: sc.Variable,
     ):
         # TODO add case where particle overcomes opposite acceleration step
         tof = tof.to(unit="s")
+        tof_accel = tof_accel.to(unit="s")
+        tof_drift = tof_drift.to(unit="s")
         v_0 = (momentum_longitudinal / mass).to(unit="m/s")
-        tof_acceleration = self.tof_in_acceleration_part(momentum_longitudinal, mass, charge).to(
-            unit="s"
-        )
-        acceleration = (charge * self.electric_field / mass).to(unit="m/s**2")
-        tof_drift = tof - tof_acceleration
-        final_velocity = tof_acceleration * acceleration + v_0
+        electric_force = charge * self.electric_field
+        acceleration = (electric_force / mass).to(unit="m/s**2")
+        final_velocity = tof_accel.to(unit="s") * acceleration + v_0
         z = sc.where(
             tof_drift < sc.scalar(0, unit="s"),
             acceleration * tof**2 / 2 + v_0 * tof,
-            acceleration * tof_acceleration**2 / 2
-            + v_0 * tof_acceleration
-            + final_velocity * tof_drift,
+            acceleration * tof_accel**2 / 2 + v_0 * tof_accel + final_velocity * tof_drift,
         )
         return z
 
@@ -238,9 +227,12 @@ class RemiCalculator:
         if sc.abs(self.magnetic_field).value > 0:
             omega = self.calc_omega(mass, charge)
             alpha = (tof * omega).to(unit="dimensionless") * sc.scalar(1, unit="rad")
+            alpha = alpha % (
+                sc.array(dims=alpha.dims, values=np.sign(alpha.values) * 2 * np.pi, unit="rad")
+            )
             radius = sc.sqrt(x**2 + y**2)
-            p_r = charge * self.magnetic_field * radius / sc.sin(0.5 * alpha)
-            phi = sc.atan2(x=x, y=y) + alpha / 2
+            p_r = charge * self.magnetic_field * radius / sc.sin(0.5 * alpha) / 2
+            phi = sc.atan2(x=x, y=y) - alpha / 2
             p_x_lab = p_r * sc.cos(phi)
             p_y_lab = p_r * sc.sin(phi)
             v_x = (p_x_lab / mass).to(unit="m/s") - self.v_jet
@@ -264,18 +256,18 @@ class RemiCalculator:
         charge = charge.to(unit="C")
         acceleration_direction = np.sign(charge.value * self.electric_field.value)
         if acceleration_direction > 0:
-            length_acceleration = self.length_acceleration_electron
-            length_drift = self.length_drift_electron
-        else:
             length_acceleration = self.length_acceleration_ion
             length_drift = self.length_drift_ion
+        else:
+            length_acceleration = self.length_acceleration_electron
+            length_drift = self.length_drift_electron
 
         # TODO add case where particle overcomes opposite acceleration step
         # TODO add drift case
         if length_drift > sc.scalar(0, unit="m"):
             raise NotImplementedError()
         else:
-            pz = -(mass * length_acceleration / tof).to(unit="au momentum") - (
-                tof * charge * self.electric_field / 2
-            ).to(unit="au momentum")
+            pz = (acceleration_direction * mass * length_acceleration / tof).to(
+                unit="au momentum"
+            ) - (tof * charge * self.electric_field / 2).to(unit="au momentum")
         return pz.to(unit="au momentum")

src/redcamel/remi_gui.py
@@ -969,11 +969,11 @@ class mclass:
         for detector_radius, detector_offset in [
             (
                 self.detector_diameter_electrons.get() / 2,
-                (self.length_accel_electron.get() + self.length_drift_electron.get()) * 1e3,
+                -(self.length_accel_electron.get() + self.length_drift_electron.get()) * 1e3,
             ),
             (
                 self.detector_diameter_ions.get() / 2,
-                -(self.length_accel_ion.get() + self.length_drift_ion.get()) * 1e3,
+                +(self.length_accel_ion.get() + self.length_drift_ion.get()) * 1e3,
             ),
         ]:
             detector_x = np.sin(detector_points) * detector_radius
@@ -997,13 +997,24 @@ class mclass:
             some_hit = some_hit[dim, index]
 
         tof_value = some_hit.coords["tof"]
+        tof_accel_value = some_hit.coords["tof_accel"]
         if sc.isnan(tof_value):
             return None
         n_steps = 200
         tof_range = sc.linspace("tof", start=tof_value / n_steps, stop=tof_value, num=n_steps)
+        tof_accel_range = sc.where(tof_range > tof_accel_value, tof_accel_value, tof_range)
+        tof_drift_range = sc.where(
+            tof_range > tof_accel_value, tof_range - tof_accel_value, sc.zeros_like(tof_range)
+        )
         starting_momentum = sc.broadcast(some_hit.coords["p"], dims=["tof"], shape=tof_range.shape)
         trajectory = sc.DataArray(
-            data=sc.ones_like(tof_range), coords={"p": starting_momentum, "tof": tof_range}
+            data=sc.ones_like(tof_range),
+            coords={
+                "p": starting_momentum,
+                "tof": tof_range,
+                "tof_accel": tof_accel_range,
+                "tof_drift": tof_drift_range,
+            },
         )
         trajectory = trajectory.transform_coords(
             ["x", "y", "z"], graph=particle.detector_transformation_graph
@@ -1166,11 +1177,10 @@ class mclass:
         R_limit = detector_radius * 1.2
         pos_bins = 100
         tof_bins = 200
-
         self.x_bins_electrons = sc.linspace("x", -R_limit, R_limit, pos_bins)
         self.y_bins_electrons = sc.linspace("y", -R_limit, R_limit, pos_bins)
-        self.tof_bins_electrons = sc.linspace("tof", sc.scalar(0, unit="ns"), tof_limit, tof_bins)
-        self.R_bins_electrons = sc.linspace("R", sc.scalar(0, unit="mm"), R_limit, pos_bins)
+        self.tof_bins_electrons = sc.linspace("tof", sc.scalar(0.0, unit="ns"), tof_limit, tof_bins)
+        self.R_bins_electrons = sc.linspace("R", sc.scalar(0.0, unit="mm"), R_limit, pos_bins)
 
         R_tof_hists = [
             electron.detector_hits.hist(
@@ -1266,9 +1276,9 @@ class mclass:
         remi = self.remicalculator
         test_electron = Electron(remi)
         opposite_voltage = remi.electric_field * remi.length_acceleration_ion
-        energy = opposite_voltage * test_electron.charge
+        energy = -opposite_voltage * test_electron.charge
         absolute_momentum = sc.sqrt(2 * energy * test_electron.mass).to(unit="au momentum")
-        momentum_vector = -remi.field_unitvector * absolute_momentum
+        momentum_vector = remi.field_unitvector * absolute_momentum
         test_electron.momentum_sample = sc.DataArray(
             sc.ones(dims=["p"], shape=(1,)), coords={"p": momentum_vector}
         )

test/test_remi_calculator.py
@@ -1,35 +1,37 @@
 # SPDX-FileCopyrightText: 2025 Hannes Lindenblatt
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+import numpy as np
 import pytest
 import scipp as sc
 from chemformula import ChemFormula
+from scipp import constants
 from scipp.testing import assert_allclose
 
 from redcamel.remi_calculator import RemiCalculator
-from redcamel.remi_particles import sample_coulomb_explosion
+from redcamel.remi_particles import Electron, Ion, sample_coulomb_explosion, sample_photoionization
 
 
 @pytest.fixture(
-    params=[0.0, -1.23, +4.56], ids=["no magnet", "neg. magnetic field", "pos. magnetic field"]
+    params=[0.0, -100.0, +200.0], ids=["no magnet", "neg. magnetic field", "pos. magnetic field"]
 )
-def remi_settings(request):
+def magnet_settings(request):
     return {"magnetic_field": (request.param, "G")}
 
 
 @pytest.fixture
-def remi(remi_settings):
+def remi(magnet_settings):
     kwargs = {}
-    for key, (value, unit) in remi_settings.items():
+    for key, (value, unit) in magnet_settings.items():
         kwargs[key] = sc.scalar(value=value, unit=unit)
     return RemiCalculator(
-        length_acceleration_ion=sc.scalar(0.1, unit="m"),
+        length_acceleration_ion=sc.scalar(1.0, unit="m"),
         length_drift_ion=sc.scalar(0.0, unit="m"),
-        voltage_ion=sc.scalar(200.0, unit="V"),
-        length_acceleration_electron=sc.scalar(0.2, unit="m"),
+        voltage_ion=sc.scalar(-20.0, unit="V"),
+        length_acceleration_electron=sc.scalar(1.0, unit="m"),
         length_drift_electron=sc.scalar(0.0, unit="m"),
-        voltage_electron=sc.scalar(400.0, unit="V"),
-        v_jet=sc.scalar(1000.0, unit="m/s"),
+        voltage_electron=sc.scalar(+20.0, unit="V"),
+        v_jet=sc.scalar(1.0, unit="m/s"),
         jet_direction="+x",
         field_direction="+z",
         **kwargs,
@@ -38,7 +40,7 @@ def remi(remi_settings):
 
 @pytest.fixture
 def nitrogen_coincidence(remi):
-    return sample_coulomb_explosion(
+    coulomb_explosion = sample_coulomb_explosion(
         fragment_formulas=[ChemFormula("N"), ChemFormula("N")],
         charge_counts=[1, 2],
         kinetic_energy_release=sc.scalar(3.456, unit="eV"),
@@ -46,30 +48,173 @@ def nitrogen_coincidence(remi):
         sizes={"events": 1000, "someparameter": 5, "p": 1},
         remi=remi,
     )
+    coulomb_explosion.calculate_detector_hits()
+    return coulomb_explosion
 
 
-def test_unity_transform_p_z(nitrogen_coincidence):
-    nitrogen_coincidence.calculate_detector_hits()
-    data = nitrogen_coincidence.datagroup
-    ion_name = list(data.keys())[0]
-    ion = nitrogen_coincidence.ions[ion_name]
-    hits = data[ion_name]
-    remi = ion.remi
-    converter_graph = remi.make_graph_for_momentum_calculation(ion.mass, ion.charge)
-    hits = hits.transform_coords(["p_z"], graph=converter_graph)
-    assert_allclose(hits.coords["p_z"], hits.coords["p_long"])
+@pytest.fixture
+def photoionization_coincidence(remi):
+    coin = sample_photoionization(
+        atom_formula=ChemFormula("He"),
+        binding_energy=sc.scalar(10.0, unit="eV"),
+        photon_energy=sc.scalar(15.0, unit="eV"),
+        energy_width=sc.scalar(0.15, unit="eV"),
+        sizes={"events": 1000, "someparameter": 5, "p": 1},
+        remi=remi,
+    )
+    coin.calculate_detector_hits()
+    return coin
 
 
-def test_unity_transform_p_xy(nitrogen_coincidence):
-    nitrogen_coincidence.calculate_detector_hits()
-    data = nitrogen_coincidence.datagroup
-    ion_name = list(data.keys())[0]
-    ion = nitrogen_coincidence.ions[ion_name]
-    hits = data[ion_name]
-    remi = ion.remi
+@pytest.fixture
+def coincidence_test_cases(photoionization_coincidence, nitrogen_coincidence):
+    return [photoionization_coincidence, nitrogen_coincidence]
 
-    converter_graph = remi.make_graph_for_momentum_calculation(ion.mass, ion.charge)
-    hits = hits.transform_coords(["energy"], graph=converter_graph)
-    print(hits)
-    assert_allclose(hits.coords["p_x"], hits.coords["p_jet"])
-    assert_allclose(hits.coords["p_y"], hits.coords["p_trans"])
+
+def test_unity_transform_p_xyz(coincidence_test_cases):
+    for coin in coincidence_test_cases:
+        for particle_name, particle in coin.particles.items():
+            hits = coin.datagroup[particle_name]
+            remi = particle.remi
+            converter_graph = remi.make_graph_for_momentum_calculation(
+                particle.mass, particle.charge
+            )
+            hits = hits.transform_coords(["energy"], graph=converter_graph)
+            assert_allclose(hits.coords["p_z"], hits.coords["p_long"])
+            assert_allclose(hits.coords["p_x"], hits.coords["p_jet"])
+            assert_allclose(hits.coords["p_y"], hits.coords["p_trans"])
+
+
+def test_particles_at_detector(coincidence_test_cases):
+    for coin in coincidence_test_cases:
+        for particle_name, particle in coin.particles.items():
+            hits = coin.datagroup[particle_name]
+            remi = particle.remi
+            hits = hits.transform_coords(["z"], graph=particle.detector_transformation_graph)
+            z = hits.coords["z"]
+            if particle.charge_count < 0:
+                detector_position = -(
+                    remi.length_acceleration_electron + remi.length_drift_electron
+                )
+            else:
+                detector_position = remi.length_acceleration_ion + remi.length_drift_ion
+            assert_allclose(z, sc.zeros_like(z) + detector_position)
+
+
+def test_drifting_electron():
+    remi = RemiCalculator(
+        length_acceleration_ion=sc.scalar(1.0, unit="m"),
+        length_drift_ion=sc.scalar(0.0, unit="m"),
+        voltage_ion=sc.scalar(-1e-30, unit="V"),
+        length_acceleration_electron=sc.scalar(1.0, unit="m"),
+        length_drift_electron=sc.scalar(0.0, unit="m"),
+        voltage_electron=sc.scalar(+1e-30, unit="V"),
+        v_jet=sc.scalar(0.0, unit="m/s"),
+        magnetic_field=sc.scalar(0.0, unit="G"),
+        jet_direction="+x",
+        field_direction="+z",
+    )
+    tester = Electron(remi=remi)
+    v_vec = sc.vectors(dims=["p"], values=[[1.0, 1.0, -1.0]], unit="m/s")
+    p_vec = v_vec * tester.mass
+    p_vec = p_vec.to(unit="au momentum")
+    tester.momentum_sample = sc.DataArray(data=sc.ones(sizes=p_vec.sizes), coords={"p": p_vec})
+    tester.calculate_detector_hits()
+    hits = tester.detector_hits
+    tof = hits.coords["tof"].to(unit="s")
+    x = hits.coords["x"].to(unit="m")
+    y = hits.coords["y"].to(unit="m")
+    for coord in [x, y, tof]:
+        assert_allclose(coord, sc.ones_like(coord))
+
+
+def test_drifting_ion():
+    remi = RemiCalculator(
+        length_acceleration_ion=sc.scalar(1.0, unit="m"),
+        length_drift_ion=sc.scalar(0.0, unit="m"),
+        voltage_ion=sc.scalar(-1e-30, unit="V"),
+        length_acceleration_electron=sc.scalar(1.0, unit="m"),
+        length_drift_electron=sc.scalar(0.0, unit="m"),
+        voltage_electron=sc.scalar(+1e-30, unit="V"),
+        v_jet=sc.scalar(0.0, unit="m/s"),
+        magnetic_field=sc.scalar(0.0, unit="G"),
+        jet_direction="+x",
+        field_direction="+z",
+    )
+    tester = Ion(ChemFormula("He"), +1, remi=remi)
+    v_vec = sc.vectors(dims=["p"], values=[[1.0, 1.0, 1.0]], unit="m/s")
+    p_vec = v_vec * tester.mass
+    p_vec = p_vec.to(unit="au momentum")
+    tester.momentum_sample = sc.DataArray(data=sc.ones(sizes=p_vec.sizes), coords={"p": p_vec})
+    tester.calculate_detector_hits()
+    hits = tester.detector_hits
+    tof = hits.coords["tof"].to(unit="s")
+    x = hits.coords["x"].to(unit="m")
+    y = hits.coords["y"].to(unit="m")
+    for coord in [x, y, tof]:
+        assert_allclose(coord, sc.ones_like(coord))
+
+
+def test_full_rotating_electron():
+    cyclotron_period = sc.scalar(1.0, unit="s")
+    magnetic_field = 2 * np.pi * constants.m_e / (constants.e * cyclotron_period)
+    remi = RemiCalculator(
+        length_acceleration_ion=sc.scalar(1.0, unit="m"),
+        length_drift_ion=sc.scalar(0.0, unit="m"),
+        voltage_ion=sc.scalar(-1e-30, unit="V"),
+        length_acceleration_electron=sc.scalar(1.0, unit="m"),
+        length_drift_electron=sc.scalar(0.0, unit="m"),
+        voltage_electron=sc.scalar(+1e-30, unit="V"),
+        v_jet=sc.scalar(0.0, unit="m/s"),
+        magnetic_field=magnetic_field.to(unit="G"),
+        jet_direction="+x",
+        field_direction="+z",
+    )
+    tester = Electron(remi=remi)
+    v_vec = sc.vectors(dims=["p"], values=[[1.0, 1.0, -1.0]], unit="m/s")
+    p_vec = v_vec * tester.mass
+    p_vec = p_vec.to(unit="au momentum")
+    tester.momentum_sample = sc.DataArray(data=sc.ones(sizes=p_vec.sizes), coords={"p": p_vec})
+    tester.calculate_detector_hits()
+    hits = tester.detector_hits
+    tof = hits.coords["tof"].to(unit="s")
+    x = hits.coords["x"].to(unit="m")
+    y = hits.coords["y"].to(unit="m")
+    assert_allclose(tof, sc.ones_like(tof))
+    for coord in [x, y]:
+        assert_allclose(coord, sc.zeros_like(coord), atol=sc.scalar(1e-15, unit="m"))
+
+
+def test_half_rotating_electron():
+    cyclotron_period = sc.scalar(2.0, unit="s")
+    magnetic_field = 2 * np.pi * constants.m_e / (constants.e * cyclotron_period)
+    remi = RemiCalculator(
+        length_acceleration_ion=sc.scalar(1.0, unit="m"),
+        length_drift_ion=sc.scalar(0.0, unit="m"),
+        voltage_ion=sc.scalar(-1e-30, unit="V"),
+        length_acceleration_electron=sc.scalar(1.0, unit="m"),
+        length_drift_electron=sc.scalar(0.0, unit="m"),
+        voltage_electron=sc.scalar(+1e-30, unit="V"),
+        v_jet=sc.scalar(0.0, unit="m/s"),
+        magnetic_field=magnetic_field.to(unit="G"),
+        jet_direction="+x",
+        field_direction="+z",
+    )
+    tester = Electron(remi=remi)
+    v_vec = sc.vectors(dims=["p"], values=[[1.0, 0.0, -1.0]], unit="m/s")
+    p_vec = v_vec * tester.mass
+    p_vec = p_vec.to(unit="au momentum")
+    tester.momentum_sample = sc.DataArray(data=sc.ones(sizes=p_vec.sizes), coords={"p": p_vec})
+    tester.calculate_detector_hits()
+    hits = tester.detector_hits
+    tof = hits.coords["tof"].to(unit="s")
+    x = hits.coords["x"].to(unit="m")
+    y = hits.coords["y"].to(unit="m")
+
+    assert_allclose(tof, sc.ones_like(tof))
+    assert_allclose(x, sc.zeros_like(x), atol=sc.scalar(1e-15, unit="m"))
+    cyclotron_radius = sc.sqrt(p_vec.fields.x**2 + p_vec.fields.y**2) / (
+        constants.e * magnetic_field
+    )
+    # Looking from above, negative charges go clockwise at positive magnetic field
+    assert_allclose(y, -2 * cyclotron_radius.to(unit="m"))